8da6fcaa8a11b624619b53f33b1ccc7a50057480
paullaster <paullasterokoth98@gmail.com>
2023-02-09 06:13:31 +0300

    add react files

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 const Home = () => {
-    return (
-        <div>
-            Welcom to React Router Further research  Home page
-            <Link to="/about" >About me</Link>
-        </div>
-    );
+  return (
+    <div>
+      Welcom to React Router Further research Home page
+      <div>
+        <Link to="/about">About me</Link>
+      </div>
+    </div>
+  );
 };
 export default Home;
