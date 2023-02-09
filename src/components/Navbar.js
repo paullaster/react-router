be2261106376cad8d3942cc5f8a0efdb5b9c7af8
paullaster <paullasterokoth98@gmail.com>
2023-02-09 06:58:35 +0300

    add react files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export const Navbar = () => {
+const Navbar = () => {
   return (
     <div>Navbar</div>
   )
 }
+
+export default Navbar
