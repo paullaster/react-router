48462197c64cb3823371c17d919ef47348edee7c
paullaster <paullasterokoth98@gmail.com>
2023-02-09 06:30:08 +0300

    add react files

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ const App = () =>{
 
   return (
     <BrowserRouter>
+      <nav> this is navbar</nav>
       <Routes>
         <Route path='/' element= {< Home />} />
         <Route path='about' element= {<About/>} />
         <Route path='contact' element= {<Contact/>} />
         <Route path="*" element= {<Error />} />
       </Routes>
+      <footer> this is footer</footer>
     </BrowserRouter>
   );
 }
