58cdf1fd684b4385b620baaceedb9803b07e6465
paullaster <paullasterokoth98@gmail.com>
2023-02-09 06:21:01 +0300

    add react files

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const App = () =>{
         <Route path='/' element= {< Home />} />
         <Route path='about' element= {<About/>} />
         <Route path='contact' element= {<Contact/>} />
+        <Route path="*" element= {<Error />} />
       </Routes>
     </BrowserRouter>
   );
