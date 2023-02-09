ca91b6b28988a12ca1f27f9b9dcca1b84d8f18ea
paullaster <paullasterokoth98@gmail.com>
2023-02-09 06:20:37 +0300

    add react files

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
 const About = () => {
-    return(
-        <div>
-           <h1> About Paullaster Okoth</h1>
-           <div>
+  return (
+    <div>
+      <h1> About Paullaster Okoth</h1>
+      <div>
         <Link to="/">Back home</Link>
       </div>
       <div>
         <Link to="/contact">Contact me</Link>
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 export default About;
