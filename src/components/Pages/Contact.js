175efe6b07b8a13585fbf34677cc64aa63674f21
paullaster <paullasterokoth98@gmail.com>
2023-02-09 06:15:04 +0300

    add react files

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
 const Contact = () => {
     return (
         <div>
             <h4> Holla me</h4>
+            <div>
+        <Link to="/about">About me</Link>
+      </div>
+      <div>
+        <Link to="/contact">Contact me</Link>
+      </div>
         </div>
     );
 };
