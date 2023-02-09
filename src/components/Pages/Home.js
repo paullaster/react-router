c70cc341665dce5de1eb7326b8cb8a9515983531
paullaster <paullasterokoth98@gmail.com>
2023-02-09 07:03:55 +0300

    add react files

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar";
+import Footer from "../Footer";
 const Home = () => {
   return (
     <>
@@ -7,6 +8,7 @@ const Home = () => {
       <section>
         <Outlet/>
       </section>
+      <Footer
     </>
     
   );
