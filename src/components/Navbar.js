22c2e0c7b2b8f135d366883dde34f451761e7ac3
paullaster <paullasterokoth98@gmail.com>
2023-02-09 07:00:08 +0300

    add react files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav>
-        
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/contact'>Contact</Link>
     </nav>
   )
 }
