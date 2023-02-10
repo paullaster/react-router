addcfa0c8a3973225a34f16d993c74508ebfde99
paullaster <paullasterokoth98@gmail.com>
2023-02-10 06:30:15 +0300

    add react files

--- src/data.js
+++ src/data.js
@@ -8,6 +8,7 @@ export const Users = [
         id: uuid(),
         email:'ispaullaster@gmail.com',
         age: 26,
+        country: 'Kenya',
         Image:'https://media.licdn.com/dms/image/C5603AQEyi17bwKHICA/profile-displayphoto-shrink_800_800/0/1631048953208?e=2147483647&v=beta&t=PExy_DzZb71vAXnhcfZL9PlJPIbkJYDSj7dfMuKBg2A'
     }
 ]
