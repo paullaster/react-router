a04cc50c8745aff9b0534511489ebbbe2d08a35d
paullaster <paullasterokoth98@gmail.com>
2023-02-10 05:22:55 +0300

    add react files

---
+++ src/components/StyledNavbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function StyledNavbar() {
+  return (
+    <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/contact'>Contact</Link>
+    </nav>
+  )
+}
+
+export default StyledNavbar
