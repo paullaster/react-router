a24480ae76ce6c6442ea71d98c0b597f96fff5a0
paullaster <paullasterokoth98@gmail.com>
2023-02-10 19:43:34 +0300

    add react files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home, About, Contact, Error, SharedLayout, Users, 
-  SingleUser, Login, Dashboard} from "./components/Pages";
+  SingleUser, Login, Dashboard, ProtectedRoute} from "./components/Pages";
 const App = () => {
 
   const [user, setUser] = useState("");
