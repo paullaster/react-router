7c286f8307ad322daaa9b295f16959c23698991f
paullaster <paullasterokoth98@gmail.com>
2023-02-10 16:31:37 +0300

    add react files

--- src/components/Pages/Login.js
+++ src/components/Pages/Login.js
@@ -2,10 +2,12 @@ import React, {useState} from 'react'
 import Form from '../Form';
 import InputWithLabel from '../InputWithLabel';
 import Button from '../Button';
+import { useNavigate } from 'react-router-dom';
 
 const Login = ({setUser}) => {
     const [userName, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
