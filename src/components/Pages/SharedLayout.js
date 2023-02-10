2a5a63947808a1e58bcafe227410d7f605c54719
paullaster <paullasterokoth98@gmail.com>
2023-02-10 05:29:59 +0300

    add react files

--- src/components/Pages/SharedLayout.js
+++ src/components/Pages/SharedLayout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
-import Navbar from "../Navbar";
+import Navbar from "../StyledNavbar";
 import Footer from "../Footer";
 function SharedLayout() {
   return (
