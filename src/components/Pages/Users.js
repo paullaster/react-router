9f401eb1c11c48444cb9f13012b7b46f4f84155d
paullaster <paullasterokoth98@gmail.com>
2023-02-10 20:03:58 +0300

    add react files

--- src/components/Pages/Users.js
+++ src/components/Pages/Users.js
@@ -6,30 +6,9 @@ import '../../assets/css/style.css'
 const Users = () => {
 
   return (
-   <div>
-    {
-        users.map(({id, name, age, image, profession, country}) =>{
-            return(
-                <li key={id}>
-                    <span>
-                        {name}
-                    </span>
-                    <span>
-                        <img src={image} alt={image}/>
-                    </span>
-                    <span>
-                        {country}
-                    </span>
-                    <Link to={`/users/${id}`} >View user</Link>
-                </li>
-            )
-        })
-    }
-
     <section>
         <Outlet />
     </section>
-   </div>
   )
 }
 
