104c46dbf512eb63bce6b84b5cc363780477aaa1
paullaster <paullasterokoth98@gmail.com>
2023-02-10 05:07:33 +0300

    add react files

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,6 +1,4 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "../Navbar";
-import Footer from "../Footer";
+
 const Home = () => {
   return (
     <>
--- src/components/SharedLayout.js
+++ src/components/SharedLayout.js
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
 function SharedLayout() {
   return (
     <div>SharedLayout</div>
