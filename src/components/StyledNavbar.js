6c1d86529a83dc8ef57ad3d21e6569b281ad0062
paullaster <paullasterokoth98@gmail.com>
2023-02-10 05:23:21 +0300

    add react files

--- src/components/StyledNavbar.js
+++ src/components/StyledNavbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function StyledNavbar() {
   return (
     <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/contact'>Contact</Link>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/about'>About</NavLink>
+        <NavLink to='/contact'>Contact</NavLink>
     </nav>
   )
 }
