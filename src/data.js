c6c48ef4e136c2de8d1e7ba4e9adc9c47422f728
paullaster <paullasterokoth98@gmail.com>
2023-02-10 06:29:19 +0300

    add react files

--- src/data.js
+++ src/data.js
@@ -6,6 +6,7 @@ export const Users = [
             "Software Engineer",
         ],
         id: uuid(),
+        email:'ispaullaster@gmail.com',
         Image:'https://media.licdn.com/dms/image/C5603AQEyi17bwKHICA/profile-displayphoto-shrink_800_800/0/1631048953208?e=2147483647&v=beta&t=PExy_DzZb71vAXnhcfZL9PlJPIbkJYDSj7dfMuKBg2A'
     }
 ]
