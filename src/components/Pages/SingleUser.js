12971fd736b79fe2b0502008e6e8eb34caf981b8
paullaster <paullasterokoth98@gmail.com>
2023-02-10 05:59:47 +0300

    add react files

--- src/components/Pages/SingleUser.js
+++ src/components/Pages/SingleUser.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 function SingleUser() {
+    const {userId} = useParams();
   return (
-    <div>SingleUser</div>
+    <>
+        <span>
+            userId: {}
+        </span>
+    </>
   )
 }
 
