a97a7dd90802b4db599b29c8fe51acc6367f8947
paullaster <paullasterokoth98@gmail.com>
2023-02-10 15:27:41 +0300

    add react files

--- src/components/Pages/Login.js
+++ src/components/Pages/Login.js
@@ -1,11 +1,18 @@
 import React, {useState} from 'react'
+import Form from '../Form';
+import InputWithLabel from '../InputWithLabel';
+import Button from '../Button';
 
 const Login = () => {
     const [userName, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
   return (
-    <div>Login</div>
+    <Form>
+        <InputWithLabel>Username</InputWithLabel>
+        <InputWithLabel>Password</InputWithLabel>
+        <Button>Login</Button>
+    </Form>
   )
 }
 
