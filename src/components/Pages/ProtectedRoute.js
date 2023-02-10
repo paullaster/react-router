7965c8e384cdf229e7dc41161ae2ac2133d45b4c
paullaster <paullasterokoth98@gmail.com>
2023-02-10 19:47:36 +0300

    add react files

--- src/components/Pages/ProtectedRoute.js
+++ src/components/Pages/ProtectedRoute.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
 const ProtectedRoute = ({user, children}) => {
+  if(!user){
+    return <Navigate to='/'/>
+  }
   return (
     <div>ProtectedRoute</div>
   )
