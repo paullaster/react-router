a5d14f905035187552ad38defd6db5e01abee39a
paullaster <paullasterokoth98@gmail.com>
2023-02-10 06:30:54 +0300

    add react files

--- src/data.js
+++ src/data.js
@@ -11,5 +11,18 @@ export const Users = [
         country: 'Kenya',
         language: 'English',
         Image:'https://media.licdn.com/dms/image/C5603AQEyi17bwKHICA/profile-displayphoto-shrink_800_800/0/1631048953208?e=2147483647&v=beta&t=PExy_DzZb71vAXnhcfZL9PlJPIbkJYDSj7dfMuKBg2A'
+    },
+    {
+        name: 'Edward Odhiambo',
+        Professions: [
+            "Software Engineer",
+        ],
+        id: uuid(),
+        email:'ispaullaster@gmail.com',
+        age: 26,
+        country: 'Kenya',
+        language: 'English',
+        Image:'https://media.licdn.com/dms/image/C5603AQEyi17bwKHICA/profile-displayphoto-shrink_800_800/0/1631048953208?e=2147483647&v=beta&t=PExy_DzZb71vAXnhcfZL9PlJPIbkJYDSj7dfMuKBg2A'
     }
+
 ]
