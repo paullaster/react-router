e96f35fd13a61d7a1d9d70cbd97b1f73be360ed5
paullaster <paullasterokoth98@gmail.com>
2023-02-10 14:56:46 +0300

    add react files

---
+++ src/components/InputWithLabel.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const InputWithLabel = ({children}) => {
+  return (
+    <div>
+        <label>
+            
+        </label>
+    </div>
+  )
+}
+
+export default InputWithLabel
